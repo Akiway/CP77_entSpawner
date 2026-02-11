4017aae7e162fa8fa0f100063c015c3d6c5b89aa
Akiway <aurelien.klein@viacesi.fr>
2026-02-11 01:54:22 +0100

    Mesh converter feedbacks

--- modules/classes/spawn/mesh/rotatingMesh.lua
+++ modules/classes/spawn/mesh/rotatingMesh.lua
@@ -1,10 +1,7 @@
 local mesh = require("modules/classes/spawn/mesh/mesh")
-local spawnable = require("modules/classes/spawn/spawnable")
 local style = require("modules/ui/style")
 local utils = require("modules/utils/utils")
 local Cron = require("modules/utils/Cron")
-local history = require("modules/utils/history")
-local settings = require("modules/utils/settings")
 
 ---Class for worldRotatingMeshNode
 ---@class rotatingMesh : mesh
@@ -101,113 +98,7 @@ function rotatingMesh:draw()
     ImGui.SetCursorPosX(self.maxPropertyWidth)
     self.reverse = style.trackedCheckbox(self.object, "##reverse", self.reverse)
 
-    style.mutedText("Convert to")
-    ImGui.SameLine()
-    ImGui.SetCursorPosX(self.maxPropertyWidth)
-    local options = { IconGlyphs.CubeOutline .. " Static Mesh" }
-    local convertActions = { "static" }
-
-    if self:canConvertToClothMesh() then
-        table.insert(options, IconGlyphs.ReceiptOutline .. " Cloth Mesh")
-        table.insert(convertActions, "cloth")
-    end
-
-    if self:canConvertToDynamicMesh() then
-        table.insert(options, IconGlyphs.CubeSend .. " Dynamic Mesh")
-        table.insert(convertActions, "dynamic")
-    end
-
-    self.convertTarget = math.max(0, math.min(self.convertTarget, #options - 1))
-    self.convertTarget, _ = style.trackedCombo(self.object, "##rotatingMeshConverterType", self.convertTarget, options, 150)
-    style.tooltip("Select the mesh type to convert into")
-
-    ImGui.SameLine()
-    ImGui.SetCursorPosX(self.maxPropertyWidth + 150 * style.viewSize + ImGui.GetStyle().ItemSpacing.x)
-    style.pushButtonNoBG(false)
-    if ImGui.Button("Convert") then
-        if settings.skipLossyConversionWarning then
-            history.addAction(history.getElementChange(self.object))
-            local target = convertActions[self.convertTarget + 1]
-            if target == "cloth" then
-                self:convertToClothMesh()
-            elseif target == "dynamic" then
-                self:convertToDynamicMesh()
-            else
-                self:convertToStaticMesh()
-            end
-        else
-            ImGui.OpenPopup("Lossy Conversion##rotatingMeshSingle")
-        end
-    end
-
-    if ImGui.BeginPopupModal("Lossy Conversion##rotatingMeshSingle", true, ImGuiWindowFlags.AlwaysAutoResize) then
-        style.mutedText("Warning")
-        ImGui.Text("This conversion is lossy.")
-        ImGui.Text("Rotating mesh specific properties will be removed.")
-        ImGui.Text("Do you want to continue?")
-        ImGui.Dummy(0, 8 * style.viewSize)
-        local skipWarning, changed = ImGui.Checkbox("Do not ask again", settings.skipLossyConversionWarning)
-        if changed then
-            settings.skipLossyConversionWarning = skipWarning
-            settings.save()
-        end
-        ImGui.Dummy(0, 8 * style.viewSize)
-
-        if ImGui.Button("Convert") then
-            history.addAction(history.getElementChange(self.object))
-            local target = convertActions[self.convertTarget + 1]
-            if target == "cloth" then
-                self:convertToClothMesh()
-            elseif target == "dynamic" then
-                self:convertToDynamicMesh()
-            else
-                self:convertToStaticMesh()
-            end
-            ImGui.CloseCurrentPopup()
-        end
-
-        ImGui.SameLine()
-        if ImGui.Button("Cancel") then
-            ImGui.CloseCurrentPopup()
-        end
-
-        ImGui.EndPopup()
-    end
-end
-
-function rotatingMesh:convertToStaticMesh()
-    -- Ensure current rotating logic is stopped
-    self:despawn()
-
-    -- Create a mesh instance to get default values
-    local meshInstance = mesh:new()
-
-    -- Change this object's metatable so it now inherits from mesh instead of rotatingMesh
-    setmetatable(self, { __index = mesh })
-
-    -- Update properties to reflect it's now a static mesh
-    self.dataType = meshInstance.dataType
-    self.modulePath = meshInstance.modulePath
-    self.node = meshInstance.node
-    self.description = meshInstance.description
-    self.icon = meshInstance.icon
-
-    -- Update the element's icon as well
-    if self.object then
-        self.object.icon = self.icon
-    end
-
-    -- Remove rotating-mesh-specific properties
-    self.duration = nil
-    self.axis = nil
-    self.reverse = nil
-    self.axisTypes = nil
-    self.cronID = nil
-    self.hideGenerate = nil
-    self.convertTarget = 0
-
-    -- Respawn as a static mesh
-    self:respawn()
+    self:drawConversionSelector("##rotatingMeshConverterType", "Lossy Conversion##rotatingMeshSingle")
 end
 
 function rotatingMesh:export()
@@ -218,23 +109,6 @@ function rotatingMesh:export()
     data.data.rotationAxis = self.axisTypes[self.axis + 1]
 
     return data
-end
-
-function rotatingMesh:getProperties()
-    local properties = spawnable.getProperties(self)
-    table.insert(properties, {
-        id = self.node,
-        name = self.dataType,
-        defaultHeader = true,
-        draw = function()
-            self:draw()
-        end
-    })
-    return properties
-end
-
-function rotatingMesh:getGroupedProperties()
-    return spawnable.getGroupedProperties(self)
 end
 
 return rotatingMesh
--- modules/classes/spawn/physics/dynamicMesh.lua
+++ modules/classes/spawn/physics/dynamicMesh.lua
@@ -3,9 +3,6 @@ local spawnable = require("modules/classes/spawn/spawnable")
 local visualizer = require("modules/utils/visualizer")
 local style = require("modules/ui/style")
 local utils = require("modules/utils/utils")
-local history = require("modules/utils/history")
-local rotatingMesh = require("modules/classes/spawn/mesh/rotatingMesh")
-local settings = require("modules/utils/settings")
 
 ---Class for worldDynamicMeshNode
 ---@class dynamicMesh : mesh
@@ -26,7 +23,7 @@ function dynamicMesh:new()
     o.startAsleep = true
     o.hideGenerate = true
     o.forceAutoHideDistance = 150
-    o.convertTarget = 0 -- 0=Static, 1=Rotating
+    o.convertTarget = 0
 
     setmetatable(o, { __index = self })
    	return o
@@ -92,162 +89,7 @@ function dynamicMesh:draw()
     ImGui.SetCursorPosX(self.maxPropertyWidth)
     self.forceAutoHideDistance = style.trackedDragFloat(self.object, "##forceAutoHideDistance", self.forceAutoHideDistance, 0.1, 0, 1000, "%.1f")
     
-    style.mutedText("Convert to")
-    ImGui.SameLine()
-    ImGui.SetCursorPosX(self.maxPropertyWidth)
-
-    local options = {
-        IconGlyphs.CubeOutline .. " Static Mesh",
-        IconGlyphs.FormatRotate90 .. " Rotating Mesh"
-    }
-    local convertActions = { "static", "rotating" }
-
-    if self:canConvertToClothMesh() then
-        table.insert(options, IconGlyphs.ReceiptOutline .. " Cloth Mesh")
-        table.insert(convertActions, "cloth")
-    end
-
-    self.convertTarget = math.max(0, math.min(self.convertTarget, #options - 1))
-    self.convertTarget, _ = style.trackedCombo(self.object, "##converterType", self.convertTarget, options, 150)
-    style.tooltip("Select the mesh type to convert into")
-        
-    ImGui.SameLine()
-    ImGui.SetCursorPosX(self.maxPropertyWidth + 150 * style.viewSize + ImGui.GetStyle().ItemSpacing.x)
-    style.pushButtonNoBG(false)
-    if ImGui.Button("Convert") then
-        if settings.skipLossyConversionWarning then
-            history.addAction(history.getElementChange(self.object))
-            local target = convertActions[self.convertTarget + 1]
-            if target == "rotating" then
-                self:convertToRotatingMesh()
-            elseif target == "cloth" then
-                self:convertToClothMesh()
-            else
-                self:convertToStaticMesh()
-            end
-        else
-            ImGui.OpenPopup("Lossy Conversion##dynamicMeshSingle")
-        end
-    end
-
-    if ImGui.BeginPopupModal("Lossy Conversion##dynamicMeshSingle", true, ImGuiWindowFlags.AlwaysAutoResize) then
-        style.mutedText("Warning")
-        ImGui.Text("This conversion is lossy.")
-        ImGui.Text("Dynamic mesh specific properties will be removed.")
-        ImGui.Text("Do you want to continue?")
-        ImGui.Dummy(0, 8 * style.viewSize)
-        local skipWarning, changed = ImGui.Checkbox("Do not ask again", settings.skipLossyConversionWarning)
-        if changed then
-            settings.skipLossyConversionWarning = skipWarning
-            settings.save()
-        end
-        ImGui.Dummy(0, 8 * style.viewSize)
-
-        if ImGui.Button("Convert") then
-            history.addAction(history.getElementChange(self.object))
-            local target = convertActions[self.convertTarget + 1]
-            if target == "rotating" then
-                self:convertToRotatingMesh()
-            elseif target == "cloth" then
-                self:convertToClothMesh()
-            else
-                self:convertToStaticMesh()
-            end
-            ImGui.CloseCurrentPopup()
-        end
-
-        ImGui.SameLine()
-        if ImGui.Button("Cancel") then
-            ImGui.CloseCurrentPopup()
-        end
-
-        ImGui.EndPopup()
-    end
-end
-
-function dynamicMesh:convertToRotatingMesh()
-    -- Ensure current dynamic mesh is despawned
-    self:despawn()
-
-    -- Create a rotating mesh instance to get default values
-    local rotInstance = rotatingMesh:new()
-
-    -- Change this object's metatable so it now inherits from rotatingMesh
-    setmetatable(self, { __index = rotatingMesh })
-
-    -- Update properties to reflect it's now a rotating mesh
-    self.dataType = rotInstance.dataType
-    self.modulePath = rotInstance.modulePath
-    self.node = rotInstance.node
-    self.description = rotInstance.description
-    self.icon = rotInstance.icon
-
-    -- Apply rotating-specific defaults
-    self.duration = rotInstance.duration
-    self.axis = rotInstance.axis
-    self.reverse = rotInstance.reverse
-    self.axisTypes = rotInstance.axisTypes
-    self.cronID = rotInstance.cronID
-    self.hideGenerate = rotInstance.hideGenerate
-
-    -- Update the element's icon as well
-    if self.object then
-        self.object.icon = self.icon
-    end
-
-    -- Remove dynamic-mesh-specific properties
-    self.startAsleep = nil
-    self.forceAutoHideDistance = nil
-    self.convertTarget = 0
-
-    -- Respawn with the new rotating mesh properties
-    self:respawn()
-end
-
-function dynamicMesh:convertToStaticMesh()
-	-- Create a mesh instance to get default values
-	local meshInstance = mesh:new()
-	
-	-- Change this object's metatable so it now inherits from mesh instead of dynamicMesh
-	setmetatable(self, { __index = mesh })
-	
-	-- Update properties to reflect it's now a static mesh
-	self.dataType = meshInstance.dataType
-	self.modulePath = meshInstance.modulePath
-	self.node = meshInstance.node
-	self.description = meshInstance.description
-	self.icon = meshInstance.icon
-	
-	-- Update the element's icon as well
-	if self.object then
-		self.object.icon = self.icon
-	end
-	
-	-- Remove dynamic-mesh-specific properties
-	self.startAsleep = nil
-	self.forceAutoHideDistance = nil
-	self.hideGenerate = nil
-    self.convertTarget = 0
-	
-	-- Respawn with the new static mesh properties
-	self:respawn()
-end
-
-function dynamicMesh:getProperties()
-    local properties = spawnable.getProperties(self)
-    table.insert(properties, {
-        id = self.node,
-        name = self.dataType,
-        defaultHeader = true,
-        draw = function()
-            self:draw()
-        end
-    })
-    return properties
-end
-
-function dynamicMesh:getGroupedProperties()
-    return spawnable.getGroupedProperties(self)
+    self:drawConversionSelector("##dynamicMeshConverterType", "Lossy Conversion##dynamicMeshSingle")
 end
 
 function dynamicMesh:export()
