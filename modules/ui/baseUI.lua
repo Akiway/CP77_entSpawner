bcc9adaa796b63a007ba11381b45095360f449ce
Akiway <23312133+Akiway@users.noreply.github.com>
2026-07-03 19:25:29 +0200

    a.1.3.3

--- modules/ui/baseUI.lua
+++ modules/ui/baseUI.lua
@@ -1,6 +1,6 @@
 local CodewareVersion = "1.18.0"
 local ArchiveXLVersion = "1.26.0"
-local ModVersion = "a.1.3.2"
+local ModVersion = "a.1.3.3"
 local ignoreRequirements = false
 
 local settings = require("modules/utils/settings")
